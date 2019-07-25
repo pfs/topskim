7acf6386de1e3fc4a2ef883ff1bed8f66c55f033
mdunser <marc.dunser@cern.ch>
2019-07-25 13:54:57 +0200

    submission of the skimming to condor. runs the three filters in one go

---
+++ scripts/skimScript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## make local directory
+echo 'i am in this directory'
+echo $PWD
+
+echo ls of this directory:
+ls ./
+
+
+echo moving to cmssw directory ${1}
+
+cd $1
+## do cmsenv and go back
+eval `scramv1 runtime -sh`
+
+echo 'moving back to directory on node'
+cd -
+
+echo 'will run the skim config with cmsRun'
+cmsRun $2
+
+
+echo 'copying the output file to where it belongs'
+cp $3 $4
+echo 'done.'
+
