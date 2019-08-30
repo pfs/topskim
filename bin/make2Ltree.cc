e389b0ea05120c312837b93df240d62def4a1d54
mdunser <marc.dunser@cern.ch>
2019-08-30 14:50:50 +0200

    changed the bugfix slightly

--- bin/make2Ltree.cc
+++ bin/make2Ltree.cc
@@ -1526,10 +1526,10 @@ int main(int argc, char* argv[])
 
       TString isoKey(cenBin<30 ? "cen" : "periph");
       isoKey+=abs(selLeptons[ilep].id)==13 ? "_169" : "_121";
-      Int_t xbin=isoEffSFs[isoKey]->GetXaxis()->FindBin( min(selLeptons[ilep].p4.Pt(), isoEffSFs[isoKey]->GetXaxis()->GetXmax()-0.01) );
+      Int_t xbin=isoEffSFs[isoKey]->GetXaxis()->FindBin( min(selLeptons[ilep].p4.Pt(), isoEffSFs[isoKey]->GetXaxis()->GetXmax()) );
       Int_t ybin=isoEffSFs[isoKey]->GetYaxis()->FindBin( min(fabs(selLeptons[ilep].p4.Eta()), isoEffSFs[isoKey]->GetYaxis()->GetXmax()) );
-      t_lepIsoSF.push_back( isoEffSFs[isoKey]->GetBinContent(xbin,ybin) );
-      t_lepIsoSFUnc.push_back( isoEffSFs[isoKey]->GetBinError(xbin,ybin) );
+      t_lepIsoSF   .push_back( isoEffSFs[isoKey]->GetBinContent(std::min(xbin,isoEffSFs[isoKey]->GetNbinsX()),ybin) ); //protect against overflow
+      t_lepIsoSFUnc.push_back( isoEffSFs[isoKey]->GetBinError  (std::min(xbin,isoEffSFs[isoKey]->GetNbinsX()),ybin) ); //protect against overflow
 
       //isolation-based indices
       bool isIso(true);
